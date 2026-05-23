49a85dd13b52a79eed09f7889b7df1bea3bedc3f
kamileg <kamil.kalicki.dev@gmail.com>
2026-05-23 13:45:21 +0200

    optimize meeting queryset.

--- app/meeting/utils.py
+++ app/meeting/utils.py
@@ -9,6 +9,8 @@ from django.db.models import Q
 def user_meetings_queryset(user):
     return (
         Meeting.objects.filter(Q(organizer=user) | Q(participants__user=user))
+        .select_related("organizer")
+        .prefetch_related("participants", "participants__user")
         .distinct()
         .order_by("-created_at")
     )
