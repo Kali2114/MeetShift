e70b312cb011fe685b3e573106e01670e2281386
kamileg <kamil.kalicki.dev@gmail.com>
2026-07-02 12:54:58 +0200

    added database backup script.

---
+++ scripts/backup_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+BACKUP_DIR="backups"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="$BACKUP_DIR/meetshift_$TIMESTAMP.sql.gz"
+
+mkdir -p "$BACKUP_DIR"
+
+docker compose -f docker-compose.prod.yml exec -T db pg_dump \
+  -U meetshift_user \
+  -d meetshift \
+  | gzip > "$BACKUP_FILE"
+
+echo "Backup created: $BACKUP_FILE"
